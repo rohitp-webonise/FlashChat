03925c4e8891888fc85a3e1cc29b483b9e78d434
rohitpatil-webonise <rohit.patil@weboniselab.com>
2022-05-06 10:41:19 +0530

    df

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Rohit Patil on 25/02/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let appName = "⚡️FlashChat"
+   static let registerSegue = "RegisterToChat"
+   static let loginSegue = "LoginToChat"
+    static let cellNibName = "MessageCell"
+    static let cellIdentifier = "ReusableCell"
+    
+    
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightpurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lightBlue = "BrandLightBlue"
+        
+        }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dataField = "date"
+    }
+}
--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -1,25 +1,62 @@
 //
 //  ChatViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages:[Message] = [
+    
+        Message(sender: "rohit@123.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "rohit@123.com", body: "What's up?"),
+        Message(sender: "a@b.com", body: "Fine. Tell me About Yourself"),
+        Message(sender: "rohit@123.com", body: "My Name Is Rohit Arun Patil,and Am from Kolhapur")
+    
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.dataSource = self
+        title = K.appName
+        navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil) ,forCellReuseIdentifier:K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func LogOutPressed(_ sender: UIBarButtonItem) {
+       
+        do{
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+            
+        } catch let signOutError as NSError {
+            print("Error signing out", signOutError)
+        }
+    }
+    
 }
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView,numberOfRowsInSection section:Int)->Int{
+        return messages.count
+    }
+
+   
+}
+
